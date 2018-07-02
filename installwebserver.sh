bfcd6518ff1f5bae9dc3ef9a81d921eefbc4aa49
Wicht Rafael <wichtr@ict-campus.net>
2018-07-02 15:33:50 +0200

    oben1

--- installwebserver.sh
+++ installwebserver.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo set -x
 sudo yum -y update
 sudo yum -y install httpd
 sudo systemctl start firewalld
 sudo systemctl enable firewalld
-sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --zone=public --add-service=http --permanent
 sudo firewall-cmd --reload
 sudo systemctl start httpd
 sudo systemctl enable httpd
